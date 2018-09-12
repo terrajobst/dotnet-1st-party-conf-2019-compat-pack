0aaf5db450b4e281787aa7ced7eadca95a77d08e
Immo Landwerth <immol@microsoft.com>
2018-09-12 09:28:42 -0700

    Initial commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Fabrikam.Logging;
+
+namespace Demo1
+{
+    class Customer
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var customer = new Customer
+            {
+                First = "Immo",
+                Last = "Landwerth"
+            };
+
+            MyLogger.Instance.WriteLine("Starting application...");
+            MyLogger.Instance.WriteLine(customer);
+        }
+    }
+}
